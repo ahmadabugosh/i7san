218b48ea25739d92aade579619f2d0bc0db3c7f7
Ahmad Abugosh <ahmadabugosh@gmail.com>
2017-06-10 12:27:00 +0400

    Heroku variables

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ const app = express();
  * Connect to MongoDB.
  */
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI||HEROKUMLAB);
 mongoose.connection.on('error', () => {
   console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
   process.exit();
